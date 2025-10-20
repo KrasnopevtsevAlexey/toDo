779bc0ff9fccb9c6730178a47fe556e76c2a37b2
Alexsey779 <fktrctq321@gmail.com>
2025-10-20 15:37:55 +0700

    commit_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ToDoList } from './pages/ToDoList'
-import './index.scss'
+import { ToDoListPage } from './pages/ToDoListPage'
+import './assets/scss/style.scss'
+import './assets/scss/normalize.scss'
 //import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToDoList/>
-  </StrictMode>,
+    <ToDoListPage/>
+  </StrictMode>
 )
